c0d97a31eed102fa5a35c60cdbd832323a0bc769
rifqiagniamubarok <rifqiagniamubarok@gmail.com>
2023-08-21 11:55:58 +0700

    save

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -9,12 +9,30 @@ import EditLink from './Link/EditLink';
 import Youtube from '@tiptap/extension-youtube';
 import GalleryModal, { ImageSelectionResult } from './GalleryModal';
 import TiptapImage from '@tiptap/extension-image';
+import axios from 'axios';
 
 interface Props {}
 
 const Editor: FC<Props> = (props): JSX.Element => {
   const [selectionRange, setSelectionRange] = useState<Range>();
   const [showGallery, setShowGallery] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [images, setImages] = useState<{ src: string }[]>([]);
+
+  const fetchImages = async () => {
+    const { data } = await axios('/api/image');
+    setImages(data.images);
+  };
+
+  const handleImageUpload = async (image: File) => {
+    setUploading(true);
+    const formData = new FormData();
+    formData.append('image', image);
+    const { data } = await axios.post('/api/image', formData);
+    setUploading(false);
+    setImages([data, ...images]);
+  };
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -65,6 +83,10 @@ const Editor: FC<Props> = (props): JSX.Element => {
     }
   }, [editor, selectionRange]);
 
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <>
       <div className="p-3 dark:bg-primary-dark bg-primary transition">
@@ -73,7 +95,14 @@ const Editor: FC<Props> = (props): JSX.Element => {
         {editor ? <EditLink editor={editor} /> : null}
         <EditorContent editor={editor} />
       </div>
-      <GalleryModal visible={showGallery} onClose={() => setShowGallery(false)} onSelect={handleImageSelection} />
+      <GalleryModal
+        images={images}
+        visible={showGallery}
+        onClose={() => setShowGallery(false)}
+        onSelect={handleImageSelection}
+        onFileSelect={handleImageUpload}
+        uploading={uploading}
+      />
     </>
   );
 };
---
+++ pages/api/image.ts
@@ -0,0 +1,64 @@
+import formidable from 'formidable';
+import { NextApiHandler } from 'next';
+import cloudinary from '@/lib/cloudinary';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const handler: NextApiHandler = (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case 'POST':
+      return uploadNewImage(req, res);
+    case 'GET':
+      return readAllImage(req, res);
+
+    default:
+      return res.status(404).send('Not Found!');
+  }
+};
+
+const uploadNewImage: NextApiHandler = (req, res) => {
+  const form = formidable();
+  form.parse(req, async (err, fields, files) => {
+    try {
+      if (err)
+        return res.status(500).json({
+          error: err.message,
+        });
+
+      const imageFile = files.image as formidable.File;
+
+      const { secure_url: url } = await cloudinary.uploader.upload(imageFile.filepath, {
+        folder: 'dev-blogs',
+      });
+
+      res.json({ src: url });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+};
+
+const readAllImage: NextApiHandler = async (req, res) => {
+  try {
+    const { resources } = await cloudinary.api.resources({
+      resource_type: 'image',
+      type: 'upload',
+      prefix: 'dev-blogs',
+    });
+
+    const images = resources.map(({ secure_url }: any) => ({
+      src: secure_url,
+    }));
+    res.json({ images });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default handler;
